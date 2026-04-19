82640bbda3a1e9d46fa0fcbfdb3011d231332d27
rakhimamadreymova-coder <rakhimamadreymova@gmail.com>
2026-04-20 00:02:04 +0500

    fix: format price from tiins to sums

--- internal/product/printer.go
+++ internal/product/printer.go
@@ -1,7 +1,7 @@
 package product
 
 import (
-	"strconv"
+	"fmt"
 	"strings"
 )
 
@@ -22,11 +22,30 @@ const (
 ====================`
 )
 
+func formatSum(tiin int) string {
+	sums := tiin / 100
+	tiins := tiin % 100
+
+	s := fmt.Sprintf("%d", sums)
+	result := ""
+	for i, ch := range s {
+		if i > 0 && (len(s)-i)%3 == 0 {
+			result += " "
+		}
+		result += string(ch)
+	}
+
+	if tiins == 0 {
+		return result
+	}
+	return fmt.Sprintf("%s.%02d", result, tiins)
+}
+
 func Converter(product Product, firstAmount int) string {
 	text := strings.ReplaceAll(template, "{name}", product.Name)
 	text = strings.ReplaceAll(text, "{brand}", product.Brand)
-	text = strings.ReplaceAll(text, "{price}", strconv.Itoa(product.Price))
-	text = strings.ReplaceAll(text, "{inStock}", strconv.FormatBool(product.InStock))
-	text = strings.ReplaceAll(text, "{firstAmount}", strconv.Itoa(firstAmount))
+	text = strings.ReplaceAll(text, "{price}", formatSum(product.Price))
+	text = strings.ReplaceAll(text, "{inStock}", fmt.Sprintf("%v", product.InStock))
+	text = strings.ReplaceAll(text, "{firstAmount}", formatSum(firstAmount))
 	return text
 }
