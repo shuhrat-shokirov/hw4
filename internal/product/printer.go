42234f3a5f278f306e674799370e654d18a680f6
Ezozabonu <zkrvezoza@gmail.com>
2026-05-04 17:42:29 +0500

    Refactor price formatting to use formatMoney function
    Updated price and firstAmount formatting in Converter function to use formatMoney. Added formatMoney function to format amounts as currency.

--- internal/product/printer.go
+++ internal/product/printer.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -25,8 +26,30 @@ const (
 func Converter(product Product, firstAmount int) string {
 	text := strings.ReplaceAll(template, "{name}", product.Name)
 	text = strings.ReplaceAll(text, "{brand}", product.Brand)
-	text = strings.ReplaceAll(text, "{price}", strconv.Itoa(product.Price))
+	text = strings.ReplaceAll(text, "{price}", formatMoney(product.Price))
 	text = strings.ReplaceAll(text, "{inStock}", strconv.FormatBool(product.InStock))
-	text = strings.ReplaceAll(text, "{firstAmount}", strconv.Itoa(firstAmount))
+	text = strings.ReplaceAll(text, "{firstAmount}", formatMoney(firstAmount))
 	return text
-}
+
+	func formatMoney(amount int) string {
+	sums := amount / 100
+	tiyin := amount % 100
+	s := strconv.Itoa(sums)
+
+	var result []byte
+	count := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		result = append([]byte{s[i]}, result...)
+		count++
+
+		if count%3 == 0 && i != 0 {
+			result = append([]byte{' '}, result...)
+		}	}
+	formatted := string(result)
+
+	if tiyin > 0 {
+		return fmt.Sprintf("%s.%02d", formatted, tiyin)
+	}
+	return formatted
+}}
